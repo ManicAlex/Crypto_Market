a1c2dc7468c1f630ff91962a019c23e6fdf5943e
ManicAlex <alex@line.ie>
2019-04-11 21:31:44 +0100

    improved api calls for the cryto exchanges
    improved the api calls to the alpha vantage to retrive the exchange
    values. no longer has an api call for each cryto now only uses two as
    the api only allows for 5 call per min and the application will have 6
    or more.Started the api call for the coin page after the coin-list
    conponant but still needs more work.

--- src/app/services/coin.service.ts
+++ src/app/services/coin.service.ts
@@ -10,52 +10,59 @@ export class CoinService {
   constructor(private http: HttpClient) { }
 
   url = environment.url;
-  coinCode = ['BTC', 'LTC'];
-
-  showbitcoin() {
-    return this.http.get(
-      `${this.url}&from_currency=${this.coinCode}&&to_currency=EUR&apikey=PXP1YAA08GRJ6YY1`
   
-      );
 
+    showAllCoins(codesign){
+      return this.http.get('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + codesign + '&&to_currency=EUR&apikey=HCA5KKCX8BDGPUEU');
     }
 
-    showbitCash() {
-      return this.http.get(
-        `${this.url}&from_currency=BCH&&to_currency=EUR&apikey=PXP1YAA08GRJ6YY1`
+    //Apikey only allows for 5 api calls per min and i have more
+    showAllCoins1(codesign){
+      return this.http.get('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + codesign + '&&to_currency=EUR&apikey=ZW1Z54B9AJQ1VM7I');
+    }
+
+
+
+    GetCoinStock(codesign){
+      return this.http.get('https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_MONTHLY&symbol=' + codesign + '&market=EUR&apikey=PXP1YAA08GRJ6YY1');
+    }
+  // showbitCash() {
+  //   return this.http.get(
+  //     `${this.url}&from_currency=BCH&&to_currency=EUR&apikey=PXP1YAA08GRJ6YY1`
     
-        );
+  //      );
   
-      }
-      showLitecoin() {
-        return this.http.get(
-          `${this.url}&from_currency=LTC&&to_currency=EUR&apikey=PXP1YAA08GRJ6YY1`
+  //   }
+  
+  // showLitecoin() {
+  //    return this.http.get(
+  //     `${this.url}&from_currency=LTC&&to_currency=EUR&apikey=PXP1YAA08GRJ6YY1`
       
-          );
+  //     );
     
-        }
+  //   }
 
-        showEthereum() {
-          return this.http.get(
-            `${this.url}&from_currency=ETH&&to_currency=EUR&apikey=PXP1YAA08GRJ6YY1`
+  // showEthereum() {
+  //    return this.http.get(
+  //     `${this.url}&from_currency=ETH&&to_currency=EUR&apikey=PXP1YAA08GRJ6YY1`
         
-            );
+  //      );
       
-          }
+  //    }
 
-            showRipple() {
-              return this.http.get(
-                `${this.url}&from_currency=XRP&&to_currency=EUR&apikey=PXP1YAA08GRJ6YY1`
+  // showRipple() {
+  //    return this.http.get(
+  //     `${this.url}&from_currency=XRP&&to_currency=EUR&apikey=PXP1YAA08GRJ6YY1`
             
-                );
+  //      );
           
-              }
+  //    }
 
-              showMonero() {
-                return this.http.get(
-                  `${this.url}&from_currency=XMR&&to_currency=EUR&apikey=PXP1YAA08GRJ6YY1`
+  // showMonero() {
+  //   return this.http.get(
+  //     `${this.url}&from_currency=XMR&&to_currency=EUR&apikey=PXP1YAA08GRJ6YY1`
               
-                  );
+  //      );
             
-                }
-      }
+    //  }
+}
