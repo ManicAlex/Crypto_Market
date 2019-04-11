2476c03c92e545a7717798a12035f09e1a914b9d
ManicAlex <alex@line.ie>
2019-04-11 22:07:55 +0100

    added value to coin route
    added /:coin value to the coin componant rout. this is so can take the
    cryto coin stock code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ListCoinsComponent}         from '../app/list-coins/list-coins.component';
+import {CoinComponent}         from '../app/coin/coin.component';
+import {NewsComponent}         from '../app/news/news.component';
+import {LocationComponent}         from '../app/location/location.component';
+import {BanksComponent}         from '../app/banks/banks.component';
 
 
 const routes: Routes = [
   {path: '',          component: ListCoinsComponent},
   {path: 'list-coins',     component: ListCoinsComponent},
-
+  {path: 'coin/:coin',     component: CoinComponent},
+  {path: 'news',     component: NewsComponent},
+  {path: 'location',     component: LocationComponent},
+  {path: 'banks',     component: BanksComponent},
+  
 ];
 
 @NgModule({
