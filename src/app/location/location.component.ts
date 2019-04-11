8d6065f52d3f778bede8266aad6c1e5428deefc4
ManicAlex <alex@line.ie>
2019-04-11 21:38:57 +0100

    bare bones of location started
    location componant has been started and is functional. at the moment it
    gets the users location and displays it on a map. this code was got from
    moodle page and in further development will allow for the user to pick a
    location if they do not wish to use their gps. once a location is picked
    it will show the banks in the area. this will be done using a google api
    and the key is already gotten.

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -10,6 +10,78 @@ export class LocationComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // Check the browser supports the geolocation API
+if (!("geolocation" in navigator)) {
+  alert('Opps!. Your device cannot be used to find your location...');
+}
+
+
+// ##############################################
+// # Basic tracking
+// ##############################################
+
+// Get and cache DOM objects
+const getLocationLink     = document.querySelector('#getLocation');
+const manualLocationInput = document.querySelector('#manualLocation');
+const feedbackDiv         = document.querySelector('#feedback');
+
+// An option for the geolocation API, which in our case is
+// also a hack to get around an issue in Chrome
+const locationServiceOpts = {enableHighAccuracy: true}
+
+
+// ##############################################
+// # Callbacks
+// ##############################################
+
+function getPositionSuccess(position) {
+
+  console.log(position);
+
+  const latitude  = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const accuracy  = position.coords.accuracy;
+
+  feedbackDiv.innerHTML = `
+    <p><strong>Latitude:</strong> ${latitude}<br>
+    <strong>Longitude:</strong> ${longitude}<br>
+    <strong>Accuracy:</strong> ${accuracy} Metres</p>
+    <p><a href='https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}'>View on Map</a></p>
+  `;
+}
+
+function getPositionFailure(err) {
+  feedbackDiv.innerHTML = 'Error retrieving location.';
+}
+
+
+// ##############################################
+// # Event Listeners
+// ##############################################
+
+// Respond to clicks on 'find my location'
+getLocationLink
+  .addEventListener('click', e => {
+
+    feedbackDiv.innerHTML = 'Looking...';
+
+    // Single-shot, get location data
+    navigator
+      .geolocation
+      .getCurrentPosition(
+        getPositionSuccess,
+        getPositionFailure,
+        locationServiceOpts
+      );
+
+  });
+
+// Respond, instead, to an entered location name
+// (this is just a mock-up)
+manualLocationInput
+  .addEventListener('change', () => {
+    feedbackDiv.innerHTML = `This feature has not been implemented.`;
+  });
   }
 
 }
