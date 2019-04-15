e4ed3ce88317ce52334c3d360df4b7b52175a346
ManicAlex <alex@line.ie>
2019-04-15 18:04:01 +0100

    change sytle to scss and api stock data for soins
    the style has been change to scss instead of sass. the data needed for
    the cryto coin stock data is no working thanks to the help of Shane.
    it gets the data from the api object and turns it into an array that can
    then be passed into chartjs. it currently only gets the high and low of
    the cryto coin but this will change in the future to get the open and
    close of the market. the object now only get the first 30 children using
    slice as it was getting 1886 back. improvements to the charts design
    will be done in the future.

--- src/app/coin/coin.component.ts
+++ src/app/coin/coin.component.ts
@@ -1,20 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { CoinService } from '../services/coin.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Chart } from 'chart.js';
+import { ChartService } from '../services/chart.service';
+import { map } from 'rxjs/operators';
+import { StockData } from '../stockdata';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-coin',
   templateUrl: './coin.component.html',
-  styleUrls: ['./coin.component.sass']
+  styleUrls: ['./coin.component.scss']
 })
 export class CoinComponent implements OnInit {
   codesign: any;
   stock: any;
+  chart: any = [];
+  low: any = [];
+  high: any = [];
+  res1: any;
+  data: any = [];
+
+  // stockData: StockData[];
+  // url = this.stock;
+  // stockDate = [];
+  // "2a. high (EUR)" = [];
+  // "3a. low (EUR)" = [];
+  
+  
+
 
   constructor(
-    private service: CoinService,
+    private service:     CoinService,
+    private service1:     ChartService,
+    private service2:     ChartService,
     private router      : Router,
-    private route       : ActivatedRoute
+    private route       : ActivatedRoute,
+    private http: HttpClient
   ) { }
 
   ngOnInit() {
@@ -24,12 +46,90 @@ export class CoinComponent implements OnInit {
     console.log(this.codesign);
 
     this.service.GetCoinStock(this.codesign)
-    .subscribe((data) => {
-      this.stock = data;
-      console.log(this.stock);
-    });
-    
-  }
-  
+    .subscribe((res1) => {
+      if(!res1){
+        return;
+      }
+      console.log(res1);
+      this.stock = res1;
 
+      console.log(this.stock['Time Series (Digital Currency Daily)']);
+
+      const allDates      = Object.keys(this.stock['Time Series (Digital Currency Daily)']).slice(0,30);
+      const allLows       = [];
+      const allHighs      = [];
+
+
+      allDates.forEach(date => {
+
+       allLows.push(this.stock['Time Series (Digital Currency Daily)'][date]['3a. low (EUR)']);
+
+       allHighs.push(this.stock['Time Series (Digital Currency Daily)'][date]['2a. high (EUR)']);
+
+      });
+      console.log(allHighs);
+
+
+      // this.http.get(this.url).subscribe((res: StockData[]) => {
+      //   res.forEach(y => {
+      //     this.stockDate.push(y.stockDate);
+      //     this.price.push(y.price);
+      //   });
+
+
+
+      // let high = this.stock['Time Series (Digital Currency Daily)'].map(() => this.stock = this.stock["2019-04-11"]['3a. high (EUR)']));
+      // let low = this.stock['Time Series (Digital Currency Daily)'].map(() => this.stock = this.stock.date['3a. low (EUR)']));
+      // let alldates = this.stock['Time Series (Digital Currency Daily)'].map( () => this.stock['2019-04-10']));
+      
+
+      this.chart = new Chart('canvas', {
+        
+        type: 'line',
+        
+        data: {
+          labels: allDates,
+          datasets: [
+            { 
+              data: allLows,
+              borderColor: "#3cba9f",
+              fill: true,
+              
+              label: 'Low'
+            },
+            { 
+              data: allHighs,
+              borderColor: "#ffcc00",
+              fill: true,
+              
+              label: 'High'
+            },
+          ]
+        },
+        options: {
+          
+          legend: {
+            display: true
+          },
+          scales: {
+            xAxes: [{
+              display: true
+            }
+          
+          
+          ],
+            yAxes: [{
+              display: true
+            }],
+          }
+        }
+      });
+    });
+  }
 }
+
+
+
+// let high = this.stock["Time Series (Digital Currency Daily)"]["2019-04-11"]['2a. high (EUR)'];
+//       let low = this.stock["Time Series (Digital Currency Daily)"]["2019-04-11"]["3a. low (EUR)"];
+//       let alldates = this.stock["Time Series (Digital Currency Daily)"]["2019-04-11"];
