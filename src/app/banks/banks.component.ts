743394f5d6e4fbdb802577d998b1d64b99a17255
ManicAlex <alex@line.ie>
2019-04-15 18:13:56 +0100

    change style sheet to scss instead of sass
    small change made of changing the style format to scss instead of sass

--- src/app/banks/banks.component.ts
+++ src/app/banks/banks.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-banks',
   templateUrl: './banks.component.html',
-  styleUrls: ['./banks.component.sass']
+  styleUrls: ['./banks.component.scss']
 })
 export class BanksComponent implements OnInit {
 
