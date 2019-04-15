383325bf6e5a6764ce01855ac410a1dc1cd8588c
ManicAlex <alex@line.ie>
2019-04-15 17:54:51 +0100

    more api exchange rates added for more cryto coins
    added new cryto coin exhange api calls to the app. this may increase
    depending on the scope of the app and to get the most popular cryto
    coins being used in this current point in time(15/4/19)

--- src/app/list-coins/list-coins.component.ts
+++ src/app/list-coins/list-coins.component.ts
@@ -2,15 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { CoinService } from '../services/coin.service';
 import { Router, ActivatedRoute } from '@angular/router';
 
+
+
 @Component({
   selector: 'app-list-coins',
   templateUrl: './list-coins.component.html',
-  styleUrls: ['./list-coins.component.sass']
+  styleUrls: ['./list-coins.component.scss']
 })
 export class ListCoinsComponent implements OnInit {
 coinsBTC: any = [];
 coinsLTC: any;
-coinsBCH: any;
+coinsDASH: any;
 coinsETH: any;
 coinsXRP: any;
 coinsXMR: any;
@@ -18,7 +20,10 @@ coinsXMR: any;
   constructor(
     private service: CoinService,
     private router      : Router,
-    private route       : ActivatedRoute
+    private route       : ActivatedRoute,
+
+    
+    
   ) { }
 
   ngOnInit() {
@@ -29,29 +34,29 @@ coinsXMR: any;
       console.log(this.coinsBTC);
     });
 
-    // this.service.showAllCoins("LTC")
-    // .subscribe((data) => {
-    //   this.coinsLTC = data;
-    //   console.log(this.coinsLTC);
-    // });
+    this.service.showAllCoins("LTC")
+    .subscribe((data) => {
+      this.coinsLTC = data;
+      console.log(this.coinsLTC);
+    });
 
-    // this.service.showAllCoins("BCH")
-    // .subscribe((data) => {
-    //   this.coinsBCH = data;
-    //   console.log(this.coinsBCH);
-    // });
+    this.service.showAllCoins("DASH")
+    .subscribe((data) => {
+      this.coinsDASH = data;
+      console.log(this.coinsDASH);
+    });
 
-    // this.service.showAllCoins1("ETH")
-    // .subscribe((data) => {
-    //   this.coinsETH = data;
-    //   console.log(this.coinsETH);
-    // });
+    this.service.showAllCoins1("ETH")
+    .subscribe((data) => {
+      this.coinsETH = data;
+      console.log(this.coinsETH);
+    });
 
-    // this.service.showAllCoins1("XRP")
-    // .subscribe((data) => {
-    //   this.coinsXRP = data;
-    //   console.log(this.coinsXRP);
-    // });
+    this.service.showAllCoins1("XRP")
+    .subscribe((data) => {
+      this.coinsXRP = data;
+      console.log(this.coinsXRP);
+    });
 
     // this.service.showAllCoins1("XMR")
     // .subscribe((data) => {
