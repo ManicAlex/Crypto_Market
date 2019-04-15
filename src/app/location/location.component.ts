a81db6899ba2e9e3dcca25338179a9f5cd6b89c4
ManicAlex <alex@line.ie>
2019-04-15 17:49:33 +0100

    the change made to this was syle from sass to scss
    the application will now use scss instead of sass.

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
-  styleUrls: ['./location.component.sass']
+  styleUrls: ['./location.component.scss']
 })
 export class LocationComponent implements OnInit {
 
