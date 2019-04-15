5a755dd7f5ee9b59c5148c9243738e76855356a1
ManicAlex <alex@line.ie>
2019-04-15 17:37:42 +0100

    chart service was create for the stock data
    this is the api call for the stock data of each cryto coin to populate
    the chart that will be added. this is only a testing file and may not be
    needed in the future

--- src/app/services/chart.service.ts
+++ src/app/services/chart.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChartService {
 
-  constructor() { }
+  constructor(private _http: HttpClient ) { }
+
+  GetCoinStock(codesign) {
+    return this._http.get('https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_MONTHLY&symbol=' + codesign + '&market=EUR&apikey=PXP1YAA08GRJ6YY1')
+    ;
+  }
 }
