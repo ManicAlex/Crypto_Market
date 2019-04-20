92b0313a61eb0e7222b3cc828f8f4dd82715bb20
ManicAlex <alex@line.ie>
2019-04-20 06:06:31 +0100

    just an api key change
    just changing the api keys as preivous ones ran out

--- src/app/services/coin.service.ts
+++ src/app/services/coin.service.ts
@@ -11,22 +11,23 @@ export class CoinService {
   constructor(private http: HttpClient) { }
 
   url = environment.url;
+  api = "ZW1Z54B9AJQ1VM7I";
   
   
 
     showAllCoins(codesign){
-      return this.http.get('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + codesign + '&&to_currency=EUR&apikey=PXP1YAA08GRJ6YY1');
+      return this.http.get('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + codesign + '&&to_currency=EUR&apikey=2RC330MOF4T41YUY');
     }
 
     //Apikey only allows for 5 api calls per min and i have more
     showAllCoins1(codesign){
-      return this.http.get('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + codesign + '&&to_currency=EUR&apikey=ZW1Z54B9AJQ1VM7I');
+      return this.http.get('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + codesign + '&&to_currency=EUR&apikey=1B15XTG6QUNM1V1Y');
     }
 
 
 
     GetCoinStock(codesign){
-      return this.http.get('https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=' + codesign + '&market=EUR&apikey=ZW1Z54B9AJQ1VM7I')
+      return this.http.get('https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_DAILY&symbol=' + codesign + '&market=EUR&apikey=1B15XTG6QUNM1V1Y')
       ;
     }
 
