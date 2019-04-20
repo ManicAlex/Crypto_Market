bbbdaa72669af97f8aed240611a6a660769b9134
ManicAlex <alex@line.ie>
2019-04-20 06:33:29 +0100

    attemped to get the goodle api map working
    tried to pass the data from the geolocation into the google maps api to
    display the users location and all the banks near them but hasnt worked
    yet will need to do more research.

--- src/app/banks/banks.component.ts
+++ src/app/banks/banks.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {LocationComponent} from '../location/location.component';
+import { HttpClient } from '@angular/common/http';
+import { GoogleBankService } from '../services/googleBank.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-banks',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banks.component.scss']
 })
 export class BanksComponent implements OnInit {
+  
+  marker:any;
+  longitude: any;
+  
 
-  constructor() { }
+  constructor(
+    private http         :HttpClient,
+    private location     :LocationComponent,
+    private service      :GoogleBankService,
+    private route        :ActivatedRoute,
+  ) { }
 
   ngOnInit() {
-  }
+    this.route.paramMap.subscribe(params => {
+      this.longitude = params.get("coin")
+    })
+    console.log(this.codesign);
+    let latitude = JSON.parse(localStorage.getItem('saved'));
+    
+    
 
+
+
+console.log(latitude);
+}
 }
