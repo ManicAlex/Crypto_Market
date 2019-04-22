ba0ae630f9af56802c37b40a9b2238a4143f5fde
ManicAlex <alex@line.ie>
2019-04-22 05:31:29 +0100

    TranslateModule, TranslateLoader and TranslateHttp
    importing TranslateModule, TranslateLoader and TranslateHttpLoader to
    the app. these will be used for the translation of english to french
    within the app. added to functions in this file as well that allow for
    the translation to take effect without having to load the whole page.
    all this will be connected to the i18n in the assets folder thats sorts
    the transtaltion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from '../app/app/app.component';
 import { LocationComponent } from './location/location.component';
@@ -9,14 +8,25 @@ import { ListCoinsComponent } from './list-coins/list-coins.component';
 import { CoinComponent } from './coin/coin.component';
 import { NewsComponent } from './news/news.component';
 import { BanksComponent } from './banks/banks.component';
-
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AgmCoreModule } from '@agm/core';
+import { HomeComponent } from './home/home.component';
+import {HttpClient} from '@angular/common/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
+
+const translateModule = TranslateModule.forRoot({loader: {
+  provide : TranslateLoader,
+  useFactory: HttpLoaderFactory,
+ deps : [HttpClient]
+ }});
 
 @NgModule({
   declarations: [
@@ -28,12 +38,15 @@ import { AgmCoreModule } from '@agm/core';
     BanksComponent,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    translateModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDEFYZV7hfzWR8flJQ_xi_WJ0hh6vDaesk'
     })
