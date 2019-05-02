7e2d45201039eb89d2e2781feb392500db9631b8
ManicAlex <alex@line.ie>
2019-05-03 00:00:06 +0100

    fixed the api call for the google maps
    created a cors proxy to the google api for nearby banks to allow it to
    work and is passing data for the lat and lng into it from the location
    component

---
+++ src/app/services/googleBank.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GoogleBankService {
+
+  constructor(private _http: HttpClient ) { }
+
+  GetBanks(lat, lng) {
+    console.log(lat);
+    console.log(lng);
+    
+    return this._http.get('http://localhost/cors_proxy/cors_proxy.php?lat=' + lat + '&lng=' + lng +'');
+    
+  }
+}
+
