d94332f6a106c19b0c543d3eda2d4e835332734d
ManicAlex <alex@line.ie>
2019-05-03 01:10:37 +0100

    translation file that contains the french of app

---
+++ src/app/stockdata.ts
@@ -0,0 +1,16 @@
+export interface StockData {
+    "Time Series (Digital Currency Daily)": {
+        stockDate: {
+            "1a. open (EUR)": number,
+            "1b. open (USD)": number,
+            "2a. high (EUR)": number,
+            "2b. high (USD)": number,
+            "3a. low (EUR)": number,
+            "3b. low (USD)": number,
+            "4a. close (EUR)": number,
+            "4b. close (USD)": number,
+            "5. volume": number,
+            "6. market cap (USD)": number
+        }
+    }
+}
