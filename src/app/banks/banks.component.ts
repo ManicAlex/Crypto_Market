f2295baa68fbe79f7c0faa857dbec0baf290a9a2
ManicAlex <alex@line.ie>
2019-05-05 16:39:21 +0100

    cleaned up code and added the spinner
    cleaned up the code make it easier to read and added the ngx spinner to
    the page on loading

--- src/app/banks/banks.component.ts
+++ src/app/banks/banks.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit      } from '@angular/core';
-import { LocationComponent      } from '../location/location.component';
 import { HttpClient             } from '@angular/common/http';
 import { GoogleBankService      } from '../services/googleBank.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { NgxSpinnerService      } from 'ngx-spinner';
 
 @Component({
   selector: 'app-banks',
@@ -21,30 +21,34 @@ export class BanksComponent implements OnInit {
     private http         :HttpClient,
     private service      :GoogleBankService,
     private route        :ActivatedRoute,
+    private spinner      :NgxSpinnerService
   ) { }
 
   ngOnInit() {
+
+    
+    this.spinner.show();
+    setTimeout(() => {
+    this.spinner.hide();
+    }, 3000);
 
     this.route.paramMap.subscribe(params =>{
       this.userLat = parseFloat(params.get("lat"))
       this.userLng = parseFloat(params.get("lng"))
     })
 
-
-
-
     this.service.GetBanks(this.userLat, this.userLng)
     .subscribe((res: any) => {
 
       
-      //https://stackoverflow.com/questions/51148469/creating-multiple-markers-with-angular-google-maps-from-an-api
-        this.return = res;
-        for(let int in this.return.results){
-        this.markers.push({
-              latt: (this.return.results[int].geometry.location.lat),
-              lngg: (this.return.results[int].geometry.location.lng)
-           })
-         }
+
+    this.return = res;
+    for(let int in this.return.results){
+    this.markers.push({
+       latt: (this.return.results[int].geometry.location.lat),
+       lngg: (this.return.results[int].geometry.location.lng)
+    })
+   }
   });
 }
 }
