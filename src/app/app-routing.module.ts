cd2d829200e3ee51d20791fe2268eb799cb01021
ManicAlex <alex@line.ie>
2019-05-05 16:26:16 +0100

    removed the news component routeing
    removed the new component from routing since it wasnt being used

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule             } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ListCoinsComponent}         from '../app/list-coins/list-coins.component';
-import {CoinComponent}         from '../app/coin/coin.component';
-import {NewsComponent}         from '../app/news/news.component';
-import {LocationComponent}         from '../app/location/location.component';
-import {BanksComponent}         from '../app/banks/banks.component';
-import {HomeComponent}         from '../app/home/home.component';
+import { ListCoinsComponent   } from '../app/list-coins/list-coins.component';
+import { CoinComponent        } from '../app/coin/coin.component';
+import { LocationComponent    } from '../app/location/location.component';
+import { BanksComponent       } from '../app/banks/banks.component';
+import { HomeComponent        } from '../app/home/home.component';
 
 
 const routes: Routes = [
   {path:  "", pathMatch:  "full",redirectTo:  "home"},
-  {path: 'home',     component: HomeComponent},
-  {path: 'home/list-coins',     component: ListCoinsComponent},
-  {path: 'list-coins',     component: ListCoinsComponent},
-  {path: 'coin/:coin',     component: CoinComponent},
-  {path: 'news',     component: NewsComponent},
-  {path: 'location',     component: LocationComponent},
+  {path: 'home',                             component: HomeComponent},
+  {path: 'home/list-coins',                  component: ListCoinsComponent},
+  {path: 'list-coins',                       component: ListCoinsComponent},
+  {path: 'coin/:coin',                       component: CoinComponent},
+  {path: 'location',                         component: LocationComponent},
   {path: 'location/:latitude/:latitude',     component: LocationComponent},
-  {path: 'banks/:lat/:lng',     component: BanksComponent},
+  {path: 'banks/:lat/:lng',                  component: BanksComponent},
   
 ];
 
