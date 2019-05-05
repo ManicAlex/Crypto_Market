c91f906aef340325d7620ca7a041c5f32591b628
ManicAlex <alex@line.ie>
2019-05-05 17:05:47 +0100

    cleaned up the code added back function
    cleaned up the code. added the angular back function to improve the
    navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit      } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { TranslateService       } from '@ngx-translate/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -14,19 +15,23 @@ export class HeaderComponent implements OnInit {
   constructor(
     private router      : Router,
     private route       : ActivatedRoute,
-    public translate    : TranslateService
+    public translate    : TranslateService,
+    private _location   : Location
   ) {
- 
-    translate.setDefaultLang('en');
+    translate.addLangs      (['En', 'Fr']);
+    translate.setDefaultLang('En');
+  }
+
+  backClicked() {
+    this._location.back();
   }
 
   ngOnInit() {
 
+    
+
 
   }
 
-  useLanguage(language: string) {
-    this.translate.use(language);
-}
 
 }
